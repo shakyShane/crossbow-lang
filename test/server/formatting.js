7ff022791c449c904b9075c4df1402449ece5ad7
Shane Osbourne <shane.osbourne8@gmail.com>
2014-10-21 22:28:48 +0100

    Added @hl fixes for formatting

--- test/server/formatting.js
+++ test/server/formatting.js
@@ -4,7 +4,7 @@ var dust    = require("../../lib/server");
 
 
 describe("Not fucking up formatting", function () {
-    it("Can remove @ helper on single line", function () {
+    it("Can remove @ helper on single line", function (done) {
 
         var input = multi(function () {/*
 Before {@shane /} After
@@ -13,9 +13,10 @@ Before {@shane /} After
 
         dust.renderSource(input, {name: "shane"}, function (err, out) {
             assert.equal(out, expected);
+            done();
         });
     });
-    it("Can remove @ helpers on multi line", function () {
+    it("Can remove @ helpers on multi line", function (done) {
 
         var input = multi(function () {/*
 Before
@@ -26,9 +27,10 @@ After
 
         dust.renderSource(input, {name: "shane"}, function (err, out) {
             assert.equal(out, expected);
+            done();
         });
     });
-    it("Can remove @ helpers on multi line (2)", function () {
+    it("Can remove @ helpers on multi line (2)", function (done) {
 
         var input = multi(function () {/*
 {@shane}
@@ -40,9 +42,10 @@ Shane is fixing templates
 
         dust.renderSource(input, {name: "shane"}, function (err, out) {
             assert.equal(out, expected);
+            done();
         });
     });
-    it("Can remove @ helpers on multi line (2)", function () {
+    it("Can remove @ helpers on multi line (2)", function (done) {
 
         var input = multi(function () {/*
 Before
@@ -52,8 +55,43 @@ Below
         var expected = "Before\nshane After\nBelow";
 
         dust.renderSource(input, {name: "shane"}, function (err, out) {
+            assert.equal(out, expected);
+            done()
+        });
+    });
+    it("Can remove @ helpers on multi line (2)", function (done) {
+
+        var input = multi(function () {/*
+Before
+{@hl}
+Inside a helper
+{/hl}
+Below
+*/})
+        var expected = "Before\nBelow";
+
+        dust.renderSource(input, {name: "shane"}, function (err, out) {
             require("d-logger")(out);
             assert.equal(out, expected);
+            done();
+        });
+    });
+    it("Can remove @ helpers on multi line (2)", function (done) {
+
+        var input = multi(function () {/*
+Before
+{@hl lang="js"}
+var shane = "awesome";
+{/hl}
+After
+         */})
+        //var expected = "Before\nBelow";
+
+        dust.renderSource(input, {name: "shane"}, function (err, out) {
+            require("d-logger")(out);
+            assert.include(out, "\nAfter");
+            //assert.equal(out, expected);
+            done();
         });
     });
 });
